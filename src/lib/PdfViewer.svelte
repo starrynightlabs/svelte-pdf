8f389440c031d80570a0d90ab3c4a74fe278e67e
Hyoungil Kim <devdivr@gmail.com>
2023-09-05 09:22:39 +0800

    Update PdfViewer.svelte
    Comment scroll button

--- src/lib/PdfViewer.svelte
+++ src/lib/PdfViewer.svelte
@@ -496,11 +496,11 @@
       </div>
     {/if}
   </div>
-  <button id="topBtn" on:click={() => window.scrollTo(0, 0)}>
+  <!--<button id="topBtn" on:click={() => window.scrollTo(0, 0)}>
     <svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
       <path d="M7 10v8h6v-8h5l-8-8-8 8h5z" />
     </svg>
-  </button>
+  </button>-->
 </div>
 
 <style>
